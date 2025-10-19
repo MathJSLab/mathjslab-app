3ccaec339daf7b30f6ae67282eda5db599d480e6
Sergio Lindau <38226617+sergiolindau@users.noreply.github.com>
2025-10-19 00:22:12 -0300

    chore(build): Update in version 1.7.0 on Sun, 19 Oct 2025 03:22:07 GMT

--- src/components/components.ts
+++ src/components/components.ts
@@ -1,4 +1,4 @@
-/* Web components. File generated at Sun, 19 Oct 2025 01:05:55 GMT. */
+/* Web components. File generated at Sun, 19 Oct 2025 03:19:47 GMT. */
 export * from './collapsible-content-panel/collapsible-content-panel.component';
 export * from './fixed-scroll-panel/fixed-scroll-panel.component';
 export * from './command-prompt/command-prompt.component';
