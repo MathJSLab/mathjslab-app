5f5c1a4354a37bdd4ab84118a33f181be87f3672
Sergio Lindau <38226617+sergiolindau@users.noreply.github.com>
2026-06-25 15:01:32 -0300

    chore(web): update dependencies and align with MathJSLab v2.0.0 Update in version 1.7.7 on Thu, 25 Jun 2026 18:01:27 GMT

--- src/components/components.ts
+++ src/components/components.ts
@@ -1,4 +1,4 @@
-/* Web components. File generated at Thu, 25 Jun 2026 17:34:29 GMT. */
+/* Web components. File generated at Thu, 25 Jun 2026 17:59:09 GMT. */
 export * from './collapsible-content-panel/collapsible-content-panel.component';
 export * from './fixed-scroll-panel/fixed-scroll-panel.component';
 export * from './command-prompt/command-prompt.component';
