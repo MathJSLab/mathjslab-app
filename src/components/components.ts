126be947982ebc7a2719ee507d22b5f045a3bebd
Sergio Lindau <38226617+sergiolindau@users.noreply.github.com>
2026-08-01 23:47:11 -0300

    feat(i18n): standardize multilingual app support

--- src/components/components.ts
+++ src/components/components.ts
@@ -1,4 +1,4 @@
-/* Web components. File generated at Sat, 01 Aug 2026 18:46:14 GMT. */
+/* Web components. File generated at Sun, 02 Aug 2026 02:44:54 GMT. */
 export * from './collapsible-content-panel/collapsible-content-panel.component';
 export * from './fixed-scroll-panel/fixed-scroll-panel.component';
 export * from './command-prompt/command-prompt.component';
