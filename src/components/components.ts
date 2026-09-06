4ef0538a37001059d45fe939607688cc3b103a29
Sergio Lindau <38226617+sergiolindau@users.noreply.github.com>
2026-09-06 13:57:55 -0300

    feat: standardize reusable app components and SEO Update in version 1.9.0 on Sun, 06 Sep 2026 16:57:50 GMT

--- src/components/components.ts
+++ src/components/components.ts
@@ -1,4 +1,4 @@
-/* Web components. File generated at Sun, 06 Sep 2026 15:43:15 GMT. */
+/* Web components. File generated at Sun, 06 Sep 2026 16:55:36 GMT. */
 export * from './collapsible-content-panel/collapsible-content-panel.component';
 export * from './fixed-scroll-panel/fixed-scroll-panel.component';
 export * from './command-prompt/command-prompt.component';
