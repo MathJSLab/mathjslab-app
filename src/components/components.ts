20a9cd06f5eb4088aca004ac026a9c196a49bb9d
Sergio Lindau <38226617+sergiolindau@users.noreply.github.com>
2026-09-12 19:24:02 -0300

    fix(app): improve dark controls and file opening Update in version 1.9.3 on Sat, 12 Sep 2026 22:23:57 GMT

--- src/components/components.ts
+++ src/components/components.ts
@@ -1,4 +1,4 @@
-/* Web components. File generated at Sat, 12 Sep 2026 17:54:30 GMT. */
+/* Web components. File generated at Sat, 12 Sep 2026 22:20:33 GMT. */
 export * from './collapsible-content-panel/collapsible-content-panel.component';
 export * from './fixed-scroll-panel/fixed-scroll-panel.component';
 export * from './command-prompt/command-prompt.component';
